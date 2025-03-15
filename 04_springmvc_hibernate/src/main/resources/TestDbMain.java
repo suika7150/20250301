73f82b4b856918eeba21759fffc8db98f8124af8
suika7150 <151140029+suika7150@users.noreply.github.com>
2025-03-15 15:27:58 +0800

    123

--- 04_springmvc_hibernate/src/main/resources/TestDbMain.java
+++ 04_springmvc_hibernate/src/main/resources/TestDbMain.java
@@ -3,7 +3,8 @@ public class TestDbMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Configuration configuration = new Configuration().configure();
+	    SessionFactory sessionFactory = configuration.buildSessionFactory();
+	    Session session = sessionFactory.openSession();
 	}
-
 }
